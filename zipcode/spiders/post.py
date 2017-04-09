ec53d0ef0fbbe0aea35ee461f0d533b6dc793355
kovloq <perdana_adhitama@yahoo.com>
2017-04-10 07:06:53 +0900

    crawl link

--- zipcode/spiders/post.py
+++ zipcode/spiders/post.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import HtmlResponse
+from scrapy.selector import Selector
 
 
 class PostSpider(scrapy.Spider):
@@ -8,7 +10,13 @@ class PostSpider(scrapy.Spider):
     start_urls = ['http://www.post.japanpost.jp/zipcode/']
 
     def parse(self, response):
-    	page = response.url.split("/")[-2]
-        filename = 'crawl-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+    	response = HtmlResponse(url='http://www.post.japanpost.jp/zipcode/', body=response.body)
+    	link=response.css('.areaTxt a').xpath('@href').extract()
+    	for url in link:
+    		scrapy.Request('http://google.com', callback=self.parse_link)
+  
+
+    def parse_link(self,response):
+    	#response = HtmlResponse(url='http://www.post.japanpost.jp/zipcode/', body=response.body)
+    	print "Test\n"
+
